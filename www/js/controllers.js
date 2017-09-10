e405e101e946759e4aa195f007916c3ce40a03d7
Michael Lindstrom <michaellindstr@gmail.com>
2017-09-10 14:31:58 -0500

    testing changeability of code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -48,7 +48,8 @@ angular.module('starter.controllers', [])
     { title: 'Dubstep', id: 3 },
     { title: 'Indie', id: 4 },
     { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+    { title: 'Cowbell', id: 6 },
+    { title: 'Country', id: 7},
   ];
 })
 
