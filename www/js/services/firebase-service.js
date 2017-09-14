fc5552024d0d108bf71ffd7af5d6494fb60d250a
Michael Lindstrom <michaellindstr@gmail.com>
2017-09-14 08:38:53 -0500

    testing registration on app

--- www/js/services/firebase-service.js
+++ www/js/services/firebase-service.js
@@ -2,22 +2,32 @@ let FirebaseService = function ($http, FireKey) {
 
     let config = FireKey.getConfig();
 
-    let initializeFirebase = function() {
+    let initializeFirebase = function () {
         firebase.initializeApp(config);
     };
 
-    let pushTextToFirebase = function(data) {
+    let pushTextToFirebase = function (data) {
         console.log('service', data);
-        $http.post('https://brews-clues-a07a1.firebaseio.com/.json', data) 
+        $http.post('https://brews-clues-a07a1.firebaseio.com/.json', data)
             .then((response) => {
                 console.log("response", response);
             })
             .catch((error) => {
                 console.log('error', error);
             });
-    }
+    };
 
-    return { initializeFirebase, pushTextToFirebase };
+    let getUsers = function () {
+        return $http.get('https://brews-clues-a07a1.firebaseio.com/users.json')
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.log("error", error);
+            });
+    };
+
+    return { initializeFirebase, pushTextToFirebase, getUsers };
 
 };
 
