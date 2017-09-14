b3cdf9a81cc3574b65ff73e49c886bd3c0f20aa9
Michael Lindstrom <michaellindstr@gmail.com>
2017-09-14 09:32:56 -0500

    re added scope.login function

--- www/js/controllers/login-controller.js
+++ www/js/controllers/login-controller.js
@@ -14,33 +14,33 @@ let LoginController = function(AuthService, FirebaseService, $timeout, $scope, $
 
     $scope.login = function (email, password) {
 
-        // AuthService.loginFirebaseUser(email, password)
-        // .then((response) => {
-        //     console.log("controller response", response);
+        AuthService.loginFirebaseUser(email, password)
+        .then((response) => {
+            console.log("controller response", response);
 
-        //     FirebaseService.getUsers()
-        //     .then((data) => {
-        //         let users = data.data;
-        //         let keys = Object.keys(users);
+            FirebaseService.getUsers()
+            .then((data) => {
+                let users = data.data;
+                let keys = Object.keys(users);
 
-        //         let correctUser;
+                let correctUser;
 
-        //         $(keys).each((index, item) => {
-        //             let thisUser = users[item];
-        //             if (thisUser.email === response.email) {
-        //                 correctUser = response.uglyID;
-        //             }
-        //         })
+                $(keys).each((index, item) => {
+                    let thisUser = users[item];
+                    if (thisUser.email === response.email) {
+                        correctUser = response.uglyID;
+                    }
+                })
 
-        //         if (correctUser !== null) {
-        //             AuthService.setCurrentUser(correctUser);
-        //             $state.go('app.brews.suggestions');
-        //         }
-        //     })
-        // })
-        // .catch((error) => {
-        //     console.log("controller error", error);
-        // });
+                if (correctUser !== null) {
+                    AuthService.setCurrentUser(correctUser);
+                    $state.go('app.brews.suggestions');
+                }
+            })
+        })
+        .catch((error) => {
+            console.log("controller error", error);
+        });
     };
 
 
